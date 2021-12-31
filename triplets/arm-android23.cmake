40bc5a51a97192a188b44bd18fa12f941f0e7e9c
Holmes Conan <holmesconan@gmail.com>
2021-12-31 16:02:01 +0800

    update

---
+++ triplets/arm-android23.cmake
@@ -0,0 +1,7 @@
+set(VCPKG_TARGET_ARCHITECTURE arm)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+# If you need to specify the ANDROID_NATIVE_API_LEVEL,
+# please use the overlay triplet and set the value below
+set(ENV{VCPKG_ANDROID_NATIVE_API_LEVEL} "23")
